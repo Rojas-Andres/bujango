30acedb0db43b54f75d8f29005091d094a26d9c2
Rojas-Andres <andresfelipe200004@gmail.com>
2023-03-19 14:21:09 -0500

    implementacion format code

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
-from setuptools import setup, find_packages
 from pathlib import Path
+
+from setuptools import find_packages, setup
 
 here = Path(__file__).resolve().parent
 
@@ -7,8 +8,8 @@ with open("README.rst", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name='bujango',
-    version='0.2.7',
+    name="bujango",
+    version="0.2.8",
     packages=find_packages(),
     classifiers=[
         "Development Status :: 5 - Production/Stable",
@@ -26,14 +27,12 @@ setup(
         "Topic :: Software Development :: Testing",
         "Topic :: Utilities",
     ],
-    install_requires=[
-        'asgiref==3.6.0','sqlparse==0.4.3'
-    ],
+    install_requires=["asgiref==3.6.0", "sqlparse==0.4.3"],
     platforms=["any"],
     keywords="Django ORM",
     url="https://github.com/Rojas-Andres/bujango",
-    author='Andres Rojas',
-    author_email='andresfelipe200004@gmail.com',
-    description='Una biblioteca que contiene solo el ORM de Django',
+    author="Andres Rojas",
+    author_email="andresfelipe200004@gmail.com",
+    description="Una biblioteca que contiene solo el ORM de Django",
     long_description=long_description,
 )
